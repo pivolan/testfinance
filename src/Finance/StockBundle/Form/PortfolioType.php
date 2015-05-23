d935904300ed6342fff79518a65a752ba4e9d4ab
pivolan <pivolan@gmail.com>
2015-05-23 15:13:50 +0400

    crud generated

---
+++ src/Finance/StockBundle/Form/PortfolioType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Finance\StockBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PortfolioType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('createdAt')
+            ->add('user')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Finance\StockBundle\Entity\Portfolio'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'finance_stockbundle_portfolio';
+    }
+}
